dd6dfdee98121530b92e927008f721414d8250fd
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-09 00:00:56 +0100

    Commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'Using of puts'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
