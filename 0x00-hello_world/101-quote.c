09613713914b65c7bd0d5ae98e09b185d8c3d897
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-09 01:22:50 +0100

    Commit 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Using of write'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
