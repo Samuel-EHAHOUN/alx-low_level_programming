5b723de4dc331cb55c407895824e3ca7a4c43245
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-10 01:19:46 +0100

    Commit 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		if ((alphabet[i] != 'q') || (alphabet[i] != 'e')
+				putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
