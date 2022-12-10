8b14b42346249d3f508f49b6f76a08001f429603
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-10 01:13:43 +0100

    Commit 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0; i < 52; i++)
+		putchar(alphabet[i]);
+	putchar('\n');
+	return (0);
+}
