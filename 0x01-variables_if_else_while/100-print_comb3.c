dcceddfd1daa27f0c8d220b7732867dc0daf68c2
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-10 03:13:44 +0100

    Commit 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,9 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		putchar(i + '0');
 		for (j = i + 1; j < 10; j++)
 		{
+			putchar(i + '0');
 			putchar(j + '0');
 			if ((i != 8) && (j != 9))
 			{
