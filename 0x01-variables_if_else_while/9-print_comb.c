8be585734988f9ff7118afd2f6973327325cbaad
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-10 02:53:34 +0100

    Commit 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,15 @@ int main(void)
 {
 	int i;
 
-	char alphabet[] = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
-
-	for (i = 0; i < 28; i++)
-		putchar(alphabet[i]);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
