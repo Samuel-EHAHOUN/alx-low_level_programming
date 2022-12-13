10e55a0a66fddbe7deafb86d5fc60dba5ecbbc10
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-13 19:14:16 +0100

    Commit 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+/**
+ * print_sign - Entry point
+ *
+ * Description: Check if n is lowercase
+ *
+ * @n: is the character to test
+ *
+ * Return: 1 if it's positive, '-1' if it's negative  and 0 if it's zero
+ */
+
+int print_sign(int n)
+{
+	if (c > 0)
+		return (1);
+	if (c < 0)
+		return (-1);
+	if (c == 0)
+		return (0);
+}
