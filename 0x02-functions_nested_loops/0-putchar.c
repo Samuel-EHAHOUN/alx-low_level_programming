e97dc727ae3dff8ab00e96a8b2cbc7dbc8fcf682
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-13 16:39:54 +0100

    Commit 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(0, "_putchar\n", 9);
+	return (0);
+}
