3931d5d6d81db56428135b2c72c421316f0f47ac
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-14 00:17:16 +0100

    Commit 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: Check if n is lowercase
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			if (hours < 10)
+			{
+				if (minutess < 10)
+				{
+					_putchar('0');
+					_putchar(hours + '0');
+					_putchar(':');
+					_putchar('0');
+					_putchar(minutes + '0');
+				}
+				else
+				{
+					_putchar('0');
+					_putchar(hours + '0');
+					_putchar(':');
+					_putchar(minutes + '0');
+				}
+			}
+			else
+			{
+				if (minutess < 10)
+				{
+					_putchar(hours + '0');
+					_putchar(':');
+					_putchar('0');
+					_putchar(minutes + '0');
+				}
+				else
+				{
+					_putchar(hours + '0');
+					_putchar(':');
+					_putchar(minutes + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+
+}
