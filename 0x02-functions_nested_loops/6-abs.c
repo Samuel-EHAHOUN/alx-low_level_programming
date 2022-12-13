f2c60cf0e67f3da82085db96d12ce978ae229483
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-13 20:48:01 +0100

    Commit 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - Entry point
+ *
+ * Description: Check if n is lowercase
+ *
+ * @n: is the character to test
+ *
+ * Return: return n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	if (n < 0)
+		return (-n);
+}
