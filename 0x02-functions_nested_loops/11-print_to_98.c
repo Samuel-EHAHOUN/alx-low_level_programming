073ae10131518acde690251f8056b67223a123c7
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-14 02:33:22 +0100

    Commit 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,20 +21,20 @@ void print_to_98(int n)
 		{
 			for (i = n; i <= 98; i++)
 			{
-				if (n == 98)
-					printf("%d", n);
+				if (i == 98)
+					printf("%d", i);
 				else
-					printf("%d, ", n);
+					printf("%d, ", i);
 			}
 		}
 		else
 		{
 			for (i = n; i >= 98; i--)
 			{
-				if (n == 98)
-					printf("%d", n);
+				if (i == 98)
+					printf("%d", i);
 				else
-					printf("%d, ", n);
+					printf("%d, ", i);
 			}
 		}
 	}
