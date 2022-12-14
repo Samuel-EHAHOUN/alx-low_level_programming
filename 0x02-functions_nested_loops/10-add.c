3b0bc6d9d987bc7faa703732c4b1c9b0efdc6d3a
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-14 02:10:43 +0100

    Commit 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: Check if n is lowercase
+ *
+ * @a: int to add
+ *
+ * @b: int to add
+ *
+ * Return: return a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
