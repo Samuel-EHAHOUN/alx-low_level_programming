3b1a1e6178fc867891ad60295a797cbc85a1251b
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-14 03:34:50 +0100

    Commit 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int som, a, b, pair;
+
+	som = 0;
+	a = 1;
+	b = 2;
+	pair = 2;
+	printf("1, 2, ");
+	do
+	{
+		if ((som % 2) == 0)
+			pair = pair + som;
+		som = a + b;
+		a = b;
+		b = som;
+	} while ( som <= 4000000);
+	printf("%ld\n", pair);
+	return (0);
+}
