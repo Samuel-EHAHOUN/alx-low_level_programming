abdfbe3658b2933c4ab6864efbcea73fef9599fe
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-14 01:27:52 +0100

    Commit 9

---
+++ 0x02-functions_nested_loops/9-times_tables.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Description: Check if n is lowercase
+ */
+
+void times_table(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			n = i * j;
+			if (n < 10)
+				_putchar(n + '0');
+			if (n >= 10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
