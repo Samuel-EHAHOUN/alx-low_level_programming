a3d070b3028a37be9c23f7865f70d4178d0616fe
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-14 01:56:06 +0100

    Commit 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,13 +15,18 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			n = i * j;
+			if ((n <10) && (i == 0) && (j == 0))
+				_putchar(n + '0');
 			if ((n < 10) && (i == 0) && (j != 0))
 			{
 				_putchar(' ');
 				_putchar(n + '0');
 			}
-			if ((n < 10) && (i != 0))
+			if ((n < 10) && (j != 0))
+			{
+				_putchar(' ');
 				_putchar(n + '0');
+			}
 			if (n >= 10)
 			{
 				_putchar((n / 10) + '0');
