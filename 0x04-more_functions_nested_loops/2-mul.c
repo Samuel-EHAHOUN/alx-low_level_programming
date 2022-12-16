eb428a19ea3e8ea7bc065777944a3bc512fa51e9
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-15 16:12:10 -0800

    Commit 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: Check if n is lowercase
+ *
+ * @a: int to add
+ *
+ * @b: int to add
+ *
+ * Return: return a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
