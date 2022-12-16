d3f5ae3d8457ce8827175d915ad38e239fc770d0
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-15 16:23:27 -0800

    Commit 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * print_numbers - euhhh
+ *
+ * Description: Check if n is lowercase
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
