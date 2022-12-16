c84ceb33492d7402d5d1ffa4977d5a35bc42423c
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-15 17:25:34 -0800

    Commit 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - euhhh
+ *
+ * @n: numbre of uderscore
+ *
+ * Description: Check if n is lowercase
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		if (i >= 1)
+		{
+			for(j = 0; j < i; j++)
+				_putchar(' ');
+		}
+		_putchar('\');
+		_putchar('\n');
+	}
+}
