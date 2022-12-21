e536c0484caf4f5719cd9cef3e9880a468fbbcfa
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-21 01:31:53 +0100

    Commit 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ *
+ * @s: the pointer
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	char *tmp = s;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	j = i;
+	while (i >= 0)
+	{
+		*s[j - i] = tmp[i];
+		i--;
+	}
+	_putchar('\n');
+}
