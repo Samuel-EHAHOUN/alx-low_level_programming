ea71a19cca5a2bb75a7b812e9a38c370155fe66e
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-21 01:07:28 +0100

    Commit 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ *
+ * @str: the pointer
+ *
+ */
+void _puts(int *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(stri[i]);
+		i++;
+	}
+}
