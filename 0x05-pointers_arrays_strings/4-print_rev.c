add156c6fd212a0161829ef5711d3c26bf8e93c4
Samuel-EHAHOUN <samagoseha@gmail.com>
2022-12-21 01:17:28 +0100

    Commit 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *
+ * @s: the pointer
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
