780b30be9a34bc2286c46ed9384d64656cc99fac
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-10 22:44:04 +0100

    Commit 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	if (size % 2 == 0)
 		n = size / 2;
 	else
-		n = (size - 1) / 2;
+		n = (size - 1) / 2 - 1;
 	for (i = n; i < size; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
