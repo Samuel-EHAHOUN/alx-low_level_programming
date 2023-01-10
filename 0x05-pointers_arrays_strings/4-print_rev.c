c7a0d57136399114d3a5752368b22a5ddb678ced
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-10 21:50:26 +0100

    Commit 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse a string
+ *
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int size = 0;
+
+	while (*(s + size) != '\0')
+	{
+		size++;
+	}
+
+	for (i = size -1; i >= 0; i--)
+		_putchar(*(str + i);
+	_putchar('\n');
+}
