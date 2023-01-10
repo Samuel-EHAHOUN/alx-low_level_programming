e9316afddcec31279eb75b46ad5b24e049c7bba1
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-10 21:08:25 +0100

    Commit 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - fonction to swap two int
+ *
+ * @a: first interger
+ *
+ * @b: second integer
+ */
+
+void swap_int(int *a, int *b);
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
