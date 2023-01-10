37db025b2ad34a0586b383b618051057ce37d12a
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-10 23:58:38 +0100

    Commit 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -25,6 +25,6 @@ char *_strcat(char *dest, char *src)
 	}
 	for (i = 0; i < size2; i++)
 		*(dest + size1 + i) = *(src + i);
-	*(dest + size1 + size2 - 1) = '\0';
+	*(dest + size1 + size2 - 2) = '\0';
 	return (dest);
 }
