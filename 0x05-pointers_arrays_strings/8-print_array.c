5f128cfaa7e008fe86a19c04cf0b06e84fc8fd8a
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-10 23:10:31 +0100

    Commit 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,22 @@
 void print_array(int *a, int n)
 {
 	int i = 0;
+	int size = 0;
 
-	if (n == 1)
-		printf("%d\n", *(a + n - 1));
-	else
+	while (*(a + size) != '\0')
 	{
-		for (i = 0; i < n - 1; i++)
-			printf("%d, ", *(a + i));
-		printf("%d\n", *(a + n - 1));
+		size++;
+	}
+
+	if (n == size)
+	{
+		if (n == 1)
+			printf("%d\n", *(a + n - 1));
+		else
+		{
+			for (i = 0; i < n - 1; i++)
+				printf("%d, ", *(a + i));
+			printf("%d\n", *(a + n - 1));
+		}
 	}
 }
