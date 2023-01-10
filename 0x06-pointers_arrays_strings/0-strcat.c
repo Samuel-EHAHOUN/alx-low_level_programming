c157ac2158f7387ed127645c50ed65cf75da4b77
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-10 23:52:05 +0100

    Commit 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ *
+ * @dest: destination string
+ *
+ * @src: source string
+ *
+ * Return: return a char *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int size1, size2, i;
+
+	size1 = size2 = i = 0;
+	while (*(dest + size1) != '\0')
+	{
+		size1++;
+	}
+	while (*(src + size2) != '\0')
+	{
+		size2++;
+	}
+	for (i = 0, i < size2; i++)
+		*(dest + size1 + i) = *(src + i);
+	*(dest + size1 + size2 - 1) = '\0';
+	return (dest);
+}
