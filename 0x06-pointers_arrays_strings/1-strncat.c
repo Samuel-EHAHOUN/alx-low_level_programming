9d0322ba67211e1d318044ffe26b5be22255eb85
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-11 00:14:34 +0100

    Commit 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ *
+ * @dest: destination string
+ *
+ * @src: source string
+ *
+ * @n: euhhh
+ *
+ * Return: return a char *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int size1, size2, i;
+
+	size1 = size2 = i = 0;
+	while (*(dest + size1) != '\0')
+	{
+		size1++;
+	}
+	while (*(src + size2) != '\0')
+	{
+		size2++;
+	}
+	if (n >= size2)
+	{
+		for (i = 0; i <= size2; i++)
+			*(dest + size1 + i) = *(src + i);
+	}
+	else
+	{
+		for(i = 0; i <= n; i++)
+			*(dest + size1 + i) = *(src + i);
+	}
+	return (dest);
+}
