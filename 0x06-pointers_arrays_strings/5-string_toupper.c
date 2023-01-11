c9e5160774491db4749c9d7df6d5fa3cfa4386d5
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-11 04:20:07 +0100

    Commit 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - euhh
+ *
+ * @s: a string
+ *
+ * Return: a char *
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] <= 122 && 97 <= str[i]
+				str[i] = str[i++] - 33;
+	}
+}
