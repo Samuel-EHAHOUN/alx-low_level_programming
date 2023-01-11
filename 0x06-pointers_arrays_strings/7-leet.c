5303d4f07bf01ed7bf4b68aae329753ab6fd4e7a
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-11 05:37:46 +0100

    Commit 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - leet speak
+ *
+ * @str: the string
+ *
+ * Return: a char *
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j = 0;
+	char letters[] = "ae0tlAEOTL";
+	char numbers[] = "4307143071";
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j < 10; i++)
+		{
+			if (str[i] == letters[j])
+				str[i] = numbers[j];
+		}
+		i++;
+	}
+	return (str);
+}
