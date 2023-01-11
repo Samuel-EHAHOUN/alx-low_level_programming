1e0396baf5ea8999714103dbe71b5867793b54e9
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-11 04:49:31 +0100

    Commit 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "mainh"
+
+/**
+ * cap_string - euuh
+ *
+ * @str: a string
+ *
+ * Return: a char *
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0' && str[i + 1] != '\0')
+	{
+		if ((str[i] <= 122 && 97 <= str[i]) && (str[i + 1] < 97 && 122 < str[i + 1]))
+			str[i] -= 32;
+		i++;
+	}
+}
