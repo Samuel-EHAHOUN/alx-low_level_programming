bc9df284e1970e3837efe1ebb0d19ecf7d03563f
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-11 04:07:56 +0100

    Commit 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	char tmp[1000];
+	int tmp[1000];
 
 	for (i = 0; i < n; i++)
 		tmp[i] = a[i];
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		a[i] = tmp[n - i - 1];
 }
