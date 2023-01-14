2b03090dbaebb2c78d0f930f31bd7d840f07b3ed
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-14 20:33:34 +0100

    Commit 2

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strstr - aa
+ *
+ * @haystack: eee
+ *
+ * @needle: sss
+ *
+ * Return: a char *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int size1 = 0, size2 = 0, i, j;
+
+	while (haystack[size1] != '\0')
+		size1++;
+	while (needle[size2] != '\0')
+		size2++;
+
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - aaa
+ *
+ * @s: eeeee
+ *
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+		i++;
+	return (i);
+}
