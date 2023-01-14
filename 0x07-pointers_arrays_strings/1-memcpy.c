c2efc9efd19070326864df3bac0714a717327f17
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-14 17:29:27 +0100

    Commit 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - euhh
+ *
+ * @src: addd
+ *
+ * @n: qddd
+ *
+ * Return: a char *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
