7bfbe52ecf86c58cb8ca31bf0179e770a56f8d98
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-14 17:11:53 +0100

    Commit 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - euhhh
+ *
+ * @s: rrr
+ *
+ * @b: ttt
+ *
+ * @n: yyy
+ *
+ * Return: a char *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int _putchar(int n);
+char *memset(char *s, char b, unsigned int n);
