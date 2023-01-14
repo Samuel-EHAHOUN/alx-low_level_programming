28e0cb9a57a6b9312c593fed7ff28d6f8727e32e
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-14 18:26:32 +0100

    Commit 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - euhh
+ *
+ * @s: hnn
+ *
+ * @accept: huuu
+ *
+ * Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0, tmp, i;
+	int size1 = 0, size2 = 0;
+
+	while (s[size1] != '\0')
+		size1++;
+	while (accept[size2] != '\0')
+		size2++;
+
+	for (i = 0; i < size1; i++)
+	{
+		tmp = n;
+		for (j = 0; j < size2; j++)
+		{
+			if (s[size1] == accept[size2])
+				n += 1;
+		}
+		if (n == tmp)
+			n = 0;
+	}
+	return (n);
+}
