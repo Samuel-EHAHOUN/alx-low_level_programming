559dc7bc0c01f22a257947263355f85e5e02e8a2
Samuel-EHAHOUN <samagoseha@gmail.com>
2023-01-14 20:10:44 +0100

    Commit 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - eeeeeeee
+ *
+ * @s: a
+ *
+ * @accept: a
+ *
+ * Return: a char *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int size1 = 0, size2 = 0, i, j;
+
+	while (s[size1] != '\0')
+		size1++;
+	while (accept[size2] != '\0')
+		size2++;
+	for (i = 0; i < size1; i++)
+	{
+		for (j = 0; j < size2; j++)
+		{
+			if (s[i] == accept[j])
+				return (s +i);
+		}
+	}
+	return (NULL);
+}
